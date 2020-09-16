76be7639b0403cbef3288e0c43c2a74225e23106
FranklinUmeObi <franklinumeobi@gmail.com>
2020-09-16 23:45:53 +0100

    Added CSB Module Lookup

---
+++ Projects/CSBModuleLookup/build/precache-manifest.e6d8862b985c7ee80a279b1d039e65a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9409516db22db3e474a5f3d23aad56c3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5307101adcc6b722b69d",
+    "url": "./static/css/main.50cf0ace.chunk.css"
+  },
+  {
+    "revision": "5c148952c58c1456dd17",
+    "url": "./static/js/2.e89f6f14.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "./static/js/2.e89f6f14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5307101adcc6b722b69d",
+    "url": "./static/js/main.0e47b13e.chunk.js"
+  },
+  {
+    "revision": "cc326d4617f9e086d84e",
+    "url": "./static/js/runtime-main.3ad3f370.js"
+  }
+]);
